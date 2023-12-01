756d1eb8d05bd5a47355eed8007c0326e16b25a9
rxwa <145824698+rx-wa@users.noreply.github.com>
2023-12-01 20:21:57 +0000

    Create .js

---
+++ .js
@@ -0,0 +1,13 @@
+{
+  "name": "MessageSpoofer",
+  "version": "1.2.0",
+  "description": "Spoof messages in Discord.",
+  "authors": [
+    {
+      "name": "xD",
+      "id": "YourDiscordIDHere"
+    }
+  ],
+  "color": "#ff0069",
+  "sourceUrl": "https://raw.githubusercontent.com/rx-wa/l/main/x/MessageSpoofer.js"
+}
